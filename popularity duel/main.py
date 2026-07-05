4b3784d9764b85f4be69438d6f8c4719be81cf50
0xSamad <abdulsamadorakzai57@gmail.com>
2026-07-05 09:14:03 +0500

    fix the bug in popularity duel

--- popularity duel/main.py
+++ popularity duel/main.py
@@ -13,25 +13,32 @@ def compare(person1,person2):
 def play_game():
 
     score = 0
+    random1 = random.randint(1, len(game_data.data))
+    random2 = random.randint(1, len(game_data.data))
+    if random1 == random2:
+        random2 = random.randint(1, 50)
+    person1 = game_data.data[random1]
+    person2 = game_data.data[random2]
     while True:
         print(art.logo)
-        random1 = random.randint(1, len(game_data.data) )
-        random2 = random.randint(1, len(game_data.data ))
-        if random1 == random2:
-            random2 = random.randint(1, 50)
-        person1 = game_data.data[random1]
-        person2 = game_data.data[random2]
+
         compare(person1,person2)
 
         if person1['follower_count']> person2['follower_count']:
             correct = 'a'
         else:
             correct = 'b'
+
         choice = input("Who has more followers : A or B : ").lower()
         if choice==correct:
 
                 score+=1
                 print(f"You are right. Current Score {score}")
+                if choice=='b':
+                    person1 = person2
+                    person2 = random.choice(game_data.data)
+                    if person1 == person2:
+                        person2 = random.choice(game_data.data)
         else:
                 print("\n" * 10)
                 print(art.logo)
